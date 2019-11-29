d14cac80b53b384c8f976030f229f3491718456b
Bernardo Niebuhr <bernardo_brandaum@yahoo.com.br>
2019-11-29 18:39:29 +0100

    updated script for cutting variables

--- src/buffers_wgs84.py
+++ src/buffers_wgs84.py
@@ -22,6 +22,7 @@ python
 # import modules
 import os, fnmatch, imp
 import subprocess
+import numpy as np
 import grass.script as grass
 from grass.pygrass.modules.shortcuts import general as g
 from grass.pygrass.modules.shortcuts import vector as v
@@ -35,68 +36,63 @@ g.mapset(mapset = "variables_cut", flags = "c")
 
 #---------------------------------------
 # Import buffers
-folder_path = r'E:\_neojaguardatabase\Buffer70_zones\buffers'
-os.chdir(folder_path) # Change to this folder
-
-files = os.listdir(folder_path) # List files in the folder
-for i in files:
-    if i.endswith('gpkg'): # Select tif files
-        print i
-        name = i.replace('.gpkg', '')
-        v.in_ogr(input = i, output = name, overwrite = True) # Import maps
-
+# folder_path = r'E:\_neojaguardatabase\Buffer70_zones\buffers'
+# os.chdir(folder_path) # Change to this folder
+# 
+# files = os.listdir(folder_path) # List files in the folder
+# for i in files:
+#     if i.endswith('gpkg'): # Select tif files
+#         print i
+#         name = i.replace('.gpkg', '')
+#         v.in_ogr(input = i, output = name, overwrite = True) # Import maps
 
 #------
 # cut for buffers
 
-# maps to be exported (names within grass)
-maps = ['human_footprint_1993_1km_tif_exp', 'human_footprint_2009_1km_tif_exp',
-'Landcover_ESACCI_2015_300m_neotropic_albers_tif_exp', 'Livestock_Cattle_CC2006_AD_1km_neotropic_albers_tif_exp',
-'Neotropic_Earthenv_dem90m_tif_exp', 
-'Neotropic_Hansen_forest1_0_95percenttreecover_2000_30_tif_exp', 
-'drainage_15s_binary_tif_exp', 'water_frequency_2010_30m_tif_exp', 'water_frequency_2010_1km_tif_exp',
-'tree_plantation_type_tif_exp', 'tree_plantations_binary_tif_exp']
-
-# new simplified names when exporting
-names = []
+# variables to be exported
+pa = r'E:\_neojaguardatabase'
+os.chdir(pa)
+arq_maps = np.genfromtxt('Environmental spatial data_UTM_2019_teste.csv', delimiter = ';', dtype = None, skip_header = 1)
+maps_to_export = [arq_maps[index][0] for index in range(len(arq_maps))]
+names_maps = [arq_maps[index][1] for index in range(len(arq_maps))]
 
 # years for forest prop
-pa = r'C:\Users\bebs0001\Documents\JaguarDB\jaguar-codes\src'
-os.chdir(pa)
-arq = np.genfromtxt('YEARS_JAGUAR_FINAL.csv', skip_header = 1, delimiter = ';')
 arq = np.genfromtxt('YEARS_JAGUAR_FINAL.csv', delimiter = ';', dtype = None, skip_header = 1)
-years = [arq[index][5] for index in range(len(arq))]
-ind_cod = [arq[index][1][1:-1] for index in range(len(arq))]
+years = [arq[index][4] for index in range(len(arq))]
+ind_cod = [arq[index][0] for index in range(len(arq))]
 
 # List of buffers
-list_buffers = grass.list_grouped('vect', pattern = '*buffer*')['PERMANENT']
+list_buffers = grass.list_grouped('vect', pattern = '*buffer*')['variables_cut']
 
 # Map used as the base to align the other variables
-map_for_define_region = 'Neotropic_Hansen_percenttreecoverd_2000_wgs84'
+map_for_define_region = 'Neotropic_Hansen_percenttreecoverd_2000_wgs84@PERMANENT'
 
 # For each buffer/individual
 for i in list_buffers:
     
     print i
     
+    ind_codmatch = i.split('buffer_')[1][:-8]
     which_ind = np.where(np.char.find(ind_cod, ind_codmatch) == 0)[0][0]
     yy = years[which_ind]
     
     # define region
-    grass.run_command('g.region', vect = i, res = 30,
+    grass.run_command('g.region', vect = i, res = '0:00:00.9',
                       align = map_for_define_region, flags = 'p')
     
     # creates polygon around the region we are working in
-    v.in_region(output = 'region_'+i)
+    v.in_region(output = 'region_'+i, overwrite = True)
     
     # mask
     grass.run_command('r.mask', vector = i) # Mask for the buffer for individual i
     
     # cut variables using the mask
-    for mm in maps:
-        expr = i+'_'+mm+' = '+mm+'@PERMANENT'
-        print expr
+    counter = 0
+    for mm in maps_to_export:
+        expr = i+'_'+names_maps[counter]+' = '+mm+'@PERMANENT'
+        print expr, counter
         r.mapcalc(expr, overwrite = True)
+        counter = counter + 1
     
     # Loop for Hansen
     
@@ -105,14 +101,15 @@ for i in list_buffers:
     
     # loop to cut for each one and account for deforestation
     for tr in thresholds:
-        
         # Hansen bin
-        r.mapcalc(i+'_Neotropic_Hansen_percenttreecoverd_2000_wgs84_gt'+str(tr)+'_binary = if(Neotropic_Hansen_percenttreecoverd_2000_wgs84 > '+str(tr)+', 1, 0)', overwrite = True)
-        
+        r.mapcalc(i+'_Treecover_Hansen_2000_30m_gt'+str(tr)+'_binary = if('+i+'_Treecover_Hansen_2000_30m > '+str(tr)+', 1, 0)', overwrite = True)
         # Hansen correspondent year
-        expr = i+'_Neotropic_Hansen_percenttreecoverd_2000_wgs84_gt'+str(tr)+'_binary_year'+str(yy)+' = if('+i+'_Neotropical_Hansen_treecoverlossperyear_wgs84_2017 > 0 && '+ \
-        i+'_Neotropical_Hansen_treecoverlossperyear_wgs84_2017 < '+str(yy)+', 0, '+i+'_Neotropic_Hansen_percenttreecoverd_2000_wgs84_gt'+str(tr)+'_binary)'
-        r.mapcalc(expr, overwrite = True)
+        expr = i+'_Treecover_Hansen_2000_30m_gt'+str(tr)+'_binary_year'+str(yy)+' = if('+i+'_Treecoverloss_Hansen_2017_30m > 0 && '+ \
+        i+'_Treecoverloss_Hansen_2017_30m < '+str(yy)+', 0, '+i+'_Treecover_Hansen_2000_30m_gt'+str(tr)+'_binary)'
+        try:
+            r.mapcalc(expr, overwrite = True)
+        except:
+            pass
     
     # Remove mask
     grass.run_command('r.mask', flags = 'r')
@@ -122,3 +119,5 @@ for i in list_buffers:
 
 # remove all these, they have error 0,1,128
 Neotropic_Hansen_forest1_0_95percenttreecover_2000_wgs84_30m_tif
+
+g.remove raster pattern=* -f
